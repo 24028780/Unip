dc196e289f62e7bf284d8d4dfc1e3057900234ec
polite <politendzivalelo@gmail.com>
2025-06-09 18:26:14 +0200

    prime

---
+++ src/main/java/com/mycompany/prime/Prime.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prime;
+
+/**
+ *
+ * @author polit
+ */
+public class Prime {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
