6bffef616465c794ec90c2d275f5953e0af68a67
polite <politendzivalelo@gmail.com>
2025-06-14 05:17:48 +0200

    nestedloops

---
+++ src/main/java/com/mycompany/nestedloops/Nestedloops.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nestedloops;
+
+/**
+ *
+ * @author polit
+ */
+public class Nestedloops {
+
+    public static void main(String[] args) {
+        for(int j=0; j<5; j++){
+            System.out.print(" ");
+       for(int i=1;i<=10;i++){
+         System.out.print(i);
+
+       }
+    }
+    }
+}
+
+
