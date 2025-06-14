524c2a1ed76cda92a33b68d1ab063e601ab644cf
polite <politendzivalelo@gmail.com>
2025-06-14 05:26:28 +0200

    Loops

---
+++ src/main/java/com/mycompany/loops/Loops.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loops;
+
+/**
+ *
+ * @author polit
+ */
+public class Loops {
+
+    public static void main(String[] args) {
+       for(int i=-1;i<=-1000;i++){
+       System.out.println("i love you*");
+       }
+    }
+}
